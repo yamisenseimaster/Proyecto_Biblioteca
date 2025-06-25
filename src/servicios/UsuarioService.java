ad0bc6c49d49d2262c04361b907a7e22023acaa0
giselvel16 <giselvel16@gmail.com>
2025-06-25 19:14:17 -0300

    Update UsuarioService.java

--- src/servicios/UsuarioService.java
+++ src/servicios/UsuarioService.java
@@ -5,22 +5,22 @@ import estructuras.pilas.*;
 import modelos.Operacion;
 import modelos.Usuario;;
 
-public class UsuarioService {
-    private int cantidadUsuarios;
-    
+public class UsuarioService {  
     private Usuario[] arregloUsuarios;
+    private BinarySearchTree<Usuario> arbolUsuarios;
     private Queue<Usuario> colaPendientes;
     private Stack<Operacion> pilaOperaciones;
-
+    private int cantidadUsuarios;
 
     public UsuarioService(int capacidadUsuarios) {
         arregloUsuarios = new Usuario[capacidadUsuarios];
-        cantidadUsuarios = 0;
+        arbolUsuarios = new BinarySearchTree<>();
         colaPendientes = new Queue<>();
         pilaOperaciones = new Stack<>();
+        cantidadUsuarios = 0;
     }
 
-    public UsuarioService(int capacidadUsuarios, Queue<Usuario> colaPendientes, Stack<Operacion> pilaOperaciones) {
+    public boolean registrarUsuario(int numeroUsuario, int dni, String nombre, String direccion, int telefono) {
         arregloUsuarios = new Usuario[capacidadUsuarios];
         cantidadUsuarios = 0;
         colaPendientes = new Queue<>();
