8c2c6473d3a9e54b471cf17cfddf34afae4a37d2
giselvel16 <giselvel16@gmail.com>
2025-06-25 19:29:01 -0300

    Update UsuarioService.java

--- src/servicios/UsuarioService.java
+++ src/servicios/UsuarioService.java
@@ -20,11 +20,31 @@ public class UsuarioService {
         cantidadUsuarios = 0;
     }
 
-    public boolean registrarUsuario(int numeroUsuario, int dni, String nombre, String direccion, int telefono) {
-        arregloUsuarios = new Usuario[capacidadUsuarios];
-        cantidadUsuarios = 0;
-        colaPendientes = new Queue<>();
-        pilaOperaciones = new Stack<>();
+    public boolean registrarUsuario(int numeroUsuario, String dni, String nombre, String direccion, String telefono) {
+        if(cantidadUsuarios >= arregloUsuarios.length(){
+            return false;
+        }
+    if(buscarPorNumeroUsuario(numeroUsuario) != null){
+        return false;
+    }
+    Usuario nuevoUsuario = new Usuario(numeroUsuario, dni, nombre, direccion, telefono, 0); 
+    arregloUsuario[cantidadUsuarios++] = nuevoLibro;
+    arbolUsuarios.add(nuevoLibro);
+    return true;
+    }
+
+    public Usuario buscarPorUsuario(int numeroUsuario){
+        Usuario usuarioTemp = new Usuario(numeroUsuario, "", "", "", "", 0);
+        return arbolLibros.get(libroTemp);
     }
     
+    public int generarNumeroUsuarioUnico(){
+        int  numeroUsuario;
+        do {
+            numeroUsuario = (int) (Math.random() *1000);
+        } while (buscarPorCodigo(numeroUsuario) != null);
+        return numeroUsuario;
+    }
+
+
 }
